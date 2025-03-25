2cf342f896a0112e7327df8cc768eae61570bb8e
Amaya <amaya.harris@tcu.edu>
2025-03-25 09:31:03 -0500

    User CRUD #4 done! Close #4

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/converter/UserDtoToUserConverter.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/converter/UserDtoToUserConverter.java
@@ -1,4 +1,19 @@
-package edu.tcu.cs.hogwartsartifactsonline.hogwarts.converter;
+package edu.tcu.cs.hogwartsartifactsonline.hogwartsuser.converter;
 
-public class UserDtoToUserConverter {
+import edu.tcu.cs.hogwartsartifactsonline.hogwartsuser.HogwartsUser;
+import edu.tcu.cs.hogwartsartifactsonline.hogwartsuser.dto.UserDto;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDtoToUserConverter implements Converter<UserDto, HogwartsUser> {
+
+    @Override
+    public HogwartsUser convert(UserDto source) {
+        HogwartsUser hogwartsUser = new HogwartsUser();
+        hogwartsUser.setUsername(source.username());
+        hogwartsUser.setEnabled(source.enabled());
+        hogwartsUser.setRoles(source.roles());
+        return hogwartsUser;
+    }
 }
--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/converter/UserToUserDtoConverter.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/converter/UserToUserDtoConverter.java
@@ -1,4 +1,19 @@
-package edu.tcu.cs.hogwartsartifactsonline.hogwarts.converter;
+package edu.tcu.cs.hogwartsartifactsonline.hogwartsuser.converter;
 
-public class UserToUserDtoConverter {
-}
+import edu.tcu.cs.hogwartsartifactsonline.hogwartsuser.HogwartsUser;
+import edu.tcu.cs.hogwartsartifactsonline.hogwartsuser.dto.UserDto;
+import org.springframework.core.convert.converter.Converter;
+
+public class UserToUserDtoConverter implements Converter<HogwartsUser, UserDto> {
+
+    @Override
+    public UserDto convert(HogwartsUser source) {
+
+        final UserDto userDto = new UserDto(source.getId(),
+                source.getUsername(),
+                source.isEnabled(),
+                source.getRoles());
+        return userDto;
+    }
+    }
+
